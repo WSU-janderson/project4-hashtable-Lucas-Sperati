01998a8ef074223ef3cb80b01102fdd2444cba13
Lucas-Sperati <sperati.2@wright.edu>
2025-11-01 20:07:52 -0400

    started setting up the methods in the .cc\pp file and delaring them in the .h file

--- HashTable.cpp
+++ HashTable.cpp
@@ -1,3 +1,54 @@
 /**
  * HashTable.cpp
  */
+
+#include "HashTable.h"
+#include <iostream>
+#include <exception>
+#include <stdexcept>
+
+using namespace std;
+
+HashTable::HashTable(size_t initCapacity) {
+
+}
+
+bool HashTable::insert(std::string key, size_t value) {
+
+}
+
+bool HashTable::remove(std::string key) {
+
+}
+
+bool HashTable::contains(std::string& key) const {
+
+}
+
+std::optional<int> HashTable::get(const string& key) const {
+
+}
+
+int& HashTable::operator[](const string& key) {
+
+}
+
+std::vector<string> HashTable::keys() const {
+
+}
+
+double HashTable::alpha() const {
+
+}
+
+size_t HashTable::capacity() const {
+
+}
+
+size_t HashTable::size() const {
+
+}
+
+friend ostream& operator<<(ostream& os, const HashTable& hashtable) {
+
+}
--- HashTable.h
+++ HashTable.h
@@ -1,3 +1,30 @@
 /**
  * HashTable.h
  */
+#include <optional>
+#include <ostream>
+#include <string>
+#include <vector>
+using namespace std;
+
+class HashTable {
+    public:
+    size_t initCapacity;
+    string key;
+    size_t value;
+
+    //parameterized constructor
+    HashTable(size_t initCapacity = 8);
+
+    bool insert(std::string key, size_t value);
+
+    bool remove(std::string key);
+
+    bool contains(std::string& key) const;
+
+
+};
+
+class hashTableBucket {
+    public:
+};
